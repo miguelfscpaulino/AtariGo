c3ddf263d62582751a143d13af9eae85d966a165
Co-sta <33875585+Co-sta@users.noreply.github.com>
2018-10-25 17:57:45 +0100

    Add files via upload

--- go.py
+++ go.py
@@ -1,31 +1,109 @@
 
-class Game:
-  """Game engine class"""
-
-  def to_move(s):
-    #returns the player to move next given the state s
-    print('called to_move(s)')
-
-  def terminal_test(s):
-    #checks if state s is terminal
-    print('called terminal_test(s)')
-
-  def utility(s, p):
-    #returns payoff of state s if terminal or evaluation with respect to player
-    print('called utility(s, p)')
-
-  def actions(s):
-    #returns list of valid moves at state s
-    print('called actions(s)')
-
-  def result(s, a):
-    #returns the sucessor game state after playing move a at state s
-    print('called result(s, a)')
-
-  def load_board(s):
-    #loads board from file stream s. returns corresponding state
-    print('called load_board(s)')
 
 
-  def __init__(self):
-    print('nothing')
+class state():
+	"""docstring for state"""
+	def __init__(self, arg):
+		super(state, self).__init__()
+		self.table = table
+		self.player = player
+		
+class game():
+		
+
+	def to_move(s):
+		#returns the player to move next given the state s
+		pass
+		
+	def terminal_test(s):
+		#checks if state s is terminal
+		pass
+
+	def utility(s,p):
+ 		#returns payoff of state s if terminal or evaluation with respect to player		
+ 		pass
+
+	def actions(s):
+ 		#returns list of valid moves at state s		
+ 		pass
+
+	def result(s,a):
+		#returns the sucessor game state after playing move a at state s
+		pass
+
+
+	def load_board(s):
+		#loads board from file stream s. returns corresponding state
+		file = open(s, "r")
+		l=list(file.readline())
+		player= int(l[2])
+		mat = [[0 for x in range(int(l[0]))] for y in range(int(l[0]))] 
+
+		for i in range(int(l[0])):
+			l=list(file.readline())
+			for h in range(int(l[0])):
+				mat[i][h] =l[h]
+				pass
+			pass	
+			
+		state=state(mat,player)
+
+		return state
+
+def alphabeta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None):
+    """Search game to determine best action; use alpha-beta pruning.
+    This version cuts off search and uses an evaluation function."""
+
+    player = game.to_move(state)
+
+    # Functions used by alphabeta
+    def max_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
+        v = -infinity
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a),
+                                 alpha, beta, depth + 1))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    def min_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
+        v = infinity
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a),
+                                 alpha, beta, depth + 1))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    # Body of alphabeta_cutoff_search starts here:
+    # The default test cuts off at depth d or at a terminal state
+    cutoff_test = (cutoff_test or
+                   (lambda state, depth: depth > d or
+                    game.terminal_test(state)))
+    eval_fn = eval_fn or (lambda state: game.utility(state, player))
+    best_score = -infinity
+    beta = infinity
+    best_action = None
+    for a in game.actions(state):
+        v = min_value(game.result(state, a), best_score, beta, 1)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action
+
+
+
+
+
+
+if __name__ == '__main__':
+	import sys
+
+	g= game() 
+	g.load_board(sys.argv[1])
